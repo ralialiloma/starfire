bace22348898ac5de6e789b7c3d19cd31a3c099a
Wolltoffel <lzandianpour@stud.macromedia.de>
2024-05-17 04:30:46 +0200

    Start Sorting CPP Source Folders

---
+++ Project/Source/Project/AI/EQS/UEnvQueryTest__ReservedCoverLocs.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UEnvQueryTest__ReservedCoverLocs.h"
+#include "EnvironmentQuery/Contexts/EnvQueryContext_Querier.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_VectorBase.h"
+
+
+
+UEnvQueryTest__ReservedCoverLocs::UEnvQueryTest__ReservedCoverLocs(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Cost = EEnvTestCost::Low;
+	ValidItemType = UEnvQueryItemType_VectorBase::StaticClass();
+	ScoringEquation = EEnvTestScoreEquation::Constant;
+}
+
+void UEnvQueryTest__ReservedCoverLocs::RunTest(FEnvQueryInstance& QueryInstance) const
+{
+	
+	
+}
+
+FText UEnvQueryTest__ReservedCoverLocs::GetDescriptionTitle() const
+{
+	return FText::FromString("Close To Cover Wall");
+}
+
+FText UEnvQueryTest__ReservedCoverLocs::GetDescriptionDetails() const
+{
+	return DescribeFloatTestParams();
+}
+
---
+++ Project/Source/Project/AI/EQS/UEnvQueryTest__ReservedCoverLocs.h
@@ -0,0 +1,25 @@
+
+
+
+#pragma once
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "UEnvQueryTest__ReservedCoverLocs.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UEnvQueryTest__ReservedCoverLocs : public UEnvQueryTest
+{
+	GENERATED_UCLASS_BODY()
+	
+
+public:
+	UEnvQueryTest__ReservedCoverLocs();
+	virtual  void RunTest(FEnvQueryInstance& QueryInstance) const override;
+	virtual FText GetDescriptionTitle() const override;
+	virtual FText GetDescriptionDetails() const override;
+	
+};
